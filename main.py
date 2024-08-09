be1b7150f25399b1f2f6c53dbadb03677ab9f72d
FedFedavi <kykypyk@gmail.com>
2024-08-09 20:20:11 +0400

    Update main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+import pprint
+
+
+response = requests.get("https://api.github.com")
+#
+# print(response.status_code)
+#
+#
+# if response.ok:
+#     print("Запрос успешно выполнен")
+# else:
+#     print("Произошла ошибка")
+
+print(response.text)
+
+response_json = response.json()
+
+pprint.pprint(response_json)
+
