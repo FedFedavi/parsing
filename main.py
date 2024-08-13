03a55a258ae0c775df0c85e707ad6ba256213925
FedFedavi <kykypyk@gmail.com>
2024-08-13 21:10:28 +0400

    change lang in DZ

--- main.py
+++ main.py
@@ -1,18 +1,46 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
+from googletrans import Translator
 
-url = "http://quotes.toscrape.com/"
-responce = requests.get(url)
-html = responce.text
 
-soup = BeautifulSoup(html, "html.parser")
 
-text = soup.find_all("span", class_="text")
-print(text)
-author = soup.find_all("small", class_="author")
-print(author)
+def get_words():
+    url = "https://randomword.com/"
+    translator = Translator()
+    try:
+        responce = requests.get(url)
+        soup = BeautifulSoup(responce.content, "html.parser")
+        english_words = soup.find("div", id="random_word").text.strip()
+        ru_words = translator.translate(english_words, dest="ru").text.strip()
+        word_definition = soup.find("div", id="random_word_definition").text.strip()
+        ru_word_definition = translator.translate(word_definition, dest="ru").text.strip()
 
-for i in range(len(text)):
-    print(f"Цитата № - {i+1}")
-    print(text[i].text)
-    print(f"Автор цитаты - {author[i].text}\n")
+        return {
+            "ru_words": ru_words,
+            "ru_word_definition": ru_word_definition
+        }
+    except:
+        print("Произошла ошибка")
+
+def word_game():
+    print("Добро пожаловать в игру")
+
+    while True:
+        word_dict = get_words()
+        word = word_dict.get("ru_words")
+        word_definition = word_dict.get("ru_word_definition")
+
+        print(f"Значение слова - {word_definition}")
+        user = input("Что это за слово ?")
+
+        if user == word:
+            print("Ты прав, чувак!")
+        else:
+            print(f"Неа, это было {word}")
+
+        play_again = input("Хотите сыграть еще? y/n")
+        if play_again != "y":
+            print("Спасибо за игру")
+            break
+
+word_game()
